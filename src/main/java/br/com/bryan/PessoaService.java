669e4ec5fe8efd1e123adb9269094a19f54484a6
Bryan Machado <bryanmachado1703@icloud.com>
2025-08-11 17:24:16 -0300

    exemplo 1 de criação de um DTO ( sem o uso do reflaction).

--- src/main/java/br/com/bryan/PessoaService.java
+++ src/main/java/br/com/bryan/PessoaService.java
@@ -2,5 +2,9 @@ package br.com.bryan;
 
 public class PessoaService {
 
-
+    public PessoaDTO list(){
+        Pessoa pessoa = new PessoaRepository().list();
+        PessoaDTO pessoaDTO = new PessoaDTO(pessoa.getNome(), pessoa.getCpf());
+        return pessoaDTO;
+    }
 }
