77db3756c3f737d85f242689737ce349c2dcdc7c
Bryan Machado <bryanmachado1703@icloud.com>
2025-08-15 13:28:51 -0300

    início do método validar.

--- src/main/java/br/com/bryan/refl/Transformator.java
+++ src/main/java/br/com/bryan/refl/Transformator.java
@@ -2,6 +2,7 @@ package br.com.bryan.refl;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 
 public class Transformator {
 
@@ -10,11 +11,23 @@ public class Transformator {
             Class<?> source = input.getClass();
             Class<?> target = Class.forName(source + "DTO");
 
+            //cria dinamicamente o objeto da classe do "target"
             O targetClass = (O) target.getDeclaredConstructor().newInstance();
 
             Field[] sourceFields = source.getDeclaredFields();
             Field[] targetFields = target.getDeclaredFields();
 
+            Arrays.stream(sourceFields).forEach(sourceField ->
+                    Arrays.stream(targetFields).forEach( targetField -> validate(sourceField, targetField)));
+
             return targetClass;
+        }
+
+        private void validate(Field sourceField, Field targetField){
+            if(sourceField.getName().equals(targetField.getName())
+                    && sourceField.getType().equals(targetField.getType())){
+
+            }
+
         }
 }
