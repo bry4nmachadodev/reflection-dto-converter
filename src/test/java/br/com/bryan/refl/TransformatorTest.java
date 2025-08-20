0c6aa34f235b5903c1fe818681d7c6adedd626a4
Bryan Machado <bryanmachado1703@icloud.com>
2025-08-20 17:06:13 -0300

    criação 1 test.

--- src/test/java/br/com/bryan/refl/TransformatorTest.java
+++ src/test/java/br/com/bryan/refl/TransformatorTest.java
@@ -1,4 +1,25 @@
 package br.com.bryan.refl;
 
+import br.com.bryan.Pessoa;
+import br.com.bryan.PessoaDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+
 public class TransformatorTest {
+
+    Pessoa pessoa = new Pessoa(1, "João", "1234");
+
+    @Test
+    @DisplayName("Deve transformar e verificar")
+    public void shouldTranform() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        Transformator transformator = new Transformator();
+        PessoaDTO pessoaDTO = transformator.transform(pessoa);
+
+        Assertions.assertInstanceOf(PessoaDTO.class, pessoaDTO);
+        Assertions.assertEquals(pessoa.getNome(), pessoaDTO.getNome());
+        Assertions.assertEquals(pessoa.getCpf(), pessoaDTO.getCpf());
+    }
 }
