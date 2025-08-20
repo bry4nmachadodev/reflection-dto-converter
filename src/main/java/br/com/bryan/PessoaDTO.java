df6f215c80b4aab776832c2ba2bf981d19fa99c7
Bryan Machado <bryanmachado1703@icloud.com>
2025-08-20 17:00:01 -0300

    removendo constructor.

--- src/main/java/br/com/bryan/PessoaDTO.java
+++ src/main/java/br/com/bryan/PessoaDTO.java
@@ -5,10 +5,6 @@ public class PessoaDTO {
     private String nome;
     private String cpf;
 
-    public PessoaDTO(String nome, String cpf) {
-        this.nome = nome;
-        this.cpf = cpf;
-    }
 
     public String getNome() {
         return nome;
