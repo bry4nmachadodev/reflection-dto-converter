a3d35864acb15ffa977ed21adab8d7500e2bb957
Bryan Machado <bryanmachado1703@icloud.com>
2025-08-20 17:12:33 -0300

    criação classe endereço.

---
+++ src/main/java/br/com/bryan/Endereco.java
@@ -0,0 +1,12 @@
+package br.com.bryan;
+
+public class Endereco {
+
+    private String rua;
+    private Integer numero;
+
+    public Endereco(Integer numero, String rua) {
+        this.numero = numero;
+        this.rua = rua;
+    }
+}
